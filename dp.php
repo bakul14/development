b22fac646bd5d4eaeb583f150bd077c14793b680
bakul14 <93672136+bakul14@users.noreply.github.com>
2021-11-13 17:07:13 +0300

    Update dp.php

--- dp.php
+++ dp.php
@@ -1,10 +1,11 @@
-<?php
+<?php 
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("localhost","root","","register");
+$con = mysqli_connect("localhost","root","bakbakbak","register");
 // Check connection
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 ?>
+
